c0d5d98349c48b23c45f11bb8909391c21626093
Dev-SK01 <srik3713@gmail.com>
2024-04-23 21:20:15 +0530

    After Some Break Recall

--- mongoose/scripts/index.js
+++ mongoose/scripts/index.js
@@ -7,6 +7,16 @@ async function connectDB(){
     // connectiong to the mongoDB server
     await mongoose.connect("mongodb://127.0.0.1:27017/mongoose");
     // inserting the data based on the defined schema
-    const user = await userDB.create({name:'Sidtharth', age:20});
+    const user = await userDB.create({
+        name:'Sidtharth', 
+        age:20 , 
+        email:'sid@gmail.com',
+        createdAt:new Date().getUTCDate(),
+        hobbies : ['listning'],
+        address:{
+            state: 'Tamilnadu',
+            Pincode : 630551
+        }
+    }); 
     console.log(user);
 }
--- mongoose/scripts/userSchema.js
+++ mongoose/scripts/userSchema.js
@@ -1,8 +1,19 @@
 const mongoose  = require('mongoose')
 
+// ceating the new schema for the collection
 const userSchema = new mongoose.Schema({
     name : String,
-    age  : Number
+    age  : Number,
+    email:String,
+    createdAt: Date,
+    updatedAt : Date,
+    bestFriend : mongoose.SchemaTypes.ObjectId,
+    hobbies : [String],
+    address : {
+        state : String,
+        Pincode : Number
+    }
 });
 
+// users is collecton this collection uses the defined  userSchema
 module.exports = mongoose.model('users' , userSchema);
